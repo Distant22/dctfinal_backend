9abcbe7fbcf923ba7d7c181d6f90beee6ecb0b75
王行遠 <wangxingyuan@wangxingyuandeMacBook-Air.local>
2023-11-18 22:54:59 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from typing import Union
+from services.sentiment_service import analyze
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from models.content_model import Content
+
+app = FastAPI()
+origins = [
+    "http://localhost:3000",
+    "http://localhost"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/sentiment/")
+def read_root():
+    return {"text": "sentiment page"}
+
+@app.post("/sentiment/")
+async def read_item(content: Content):
+    return await analyze(content)
