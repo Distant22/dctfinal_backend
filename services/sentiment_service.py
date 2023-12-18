2695532bb72eb41663c8be1d11048b3a4c08341d
Distant22 <sindistant@gmail.com>
2023-12-18 18:53:37 +0800

    修正 Return Format 並刪除註解

--- services/sentiment_service.py
+++ services/sentiment_service.py
@@ -4,7 +4,6 @@ from models.content_model import Content
 async def sentiment_analyze_service(content: Content) -> None:
     
     client = language_v2.LanguageServiceClient()
-
     document_type_in_plain_text = language_v2.Document.Type.PLAIN_TEXT
 
     language_code = "zh-TW"
@@ -15,14 +14,13 @@ async def sentiment_analyze_service(content: Content) -> None:
     }
 
     encoding_type = language_v2.EncodingType.UTF8
-
     response = client.analyze_sentiment(
         request={"document": document, "encoding_type": encoding_type}
     )
-    # Get overall sentiment of the input document
+
     print(f"Document sentiment score: {response.document_sentiment.score}")
     print(f"Document sentiment magnitude: {response.document_sentiment.magnitude}")
-    # Get sentiment for all sentences in the document
+
     for sentence in response.sentences:
         print(f"Sentence text: {sentence.text.content}")
         print(f"Sentence sentiment score: {sentence.sentiment.score}")
@@ -32,5 +30,6 @@ async def sentiment_analyze_service(content: Content) -> None:
 
     print("\nResponse\n",response,"Sentiment object",response.sentences)
 
-    return response.sentences[0].sentiment.score
-
+    return {
+        "data": response.sentences[0].sentiment.score
+    }
